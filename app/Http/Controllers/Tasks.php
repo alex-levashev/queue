3ac8ed87381e8d491b8bf539fc81157366981ea6
Alexey Levashev <alex.levashev@gmail.com>
2018-11-28 20:21:46 +0100

    Making table look as a table

--- app/Http/Controllers/Tasks.php
+++ app/Http/Controllers/Tasks.php
@@ -9,11 +9,18 @@ class Tasks extends Controller
 {
     public function ShowTasks() {
 
+      echo '<table style="width:100%">
+              <tr>
+                <th>ID</th>
+                <th>Task</th>
+                <th>Count</th>
+              </tr>';
       $tasks = DB::table('tasks')->get();
       foreach ($tasks as $task) {
-        echo $task->id . ' | <a href="/tasks/' . $task->id . '">' . $task->name . '</a> | ' . $task->counter . '<br>';
-        echo '------------<br>';
+        echo '<tr>';
+        echo '<td>' . $task->id . '</td><td><a href="/tasks/' . $task->id . '">' . $task->name . '</a></td><td>' . $task->counter . '</td>';
+        echo '</tr>';
       }
-
+    echo '</table>;  
     }
 }
