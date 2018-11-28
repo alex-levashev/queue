c19fc6a3e300c80a0a219452d35d4e2776219e7d
Alexey Levashev <alex.levashev@gmail.com>
2018-11-28 20:23:54 +0100

    Error fixing

--- app/Http/Controllers/Tasks.php
+++ app/Http/Controllers/Tasks.php
@@ -9,18 +9,13 @@ class Tasks extends Controller
 {
     public function ShowTasks() {
 
-      echo '<table style="width:100%">
-              <tr>
-                <th>ID</th>
-                <th>Task</th>
-                <th>Count</th>
-              </tr>';
+      echo '<table style="width:100%"><tr><th>ID</th><th>Task</th><th>Count</th></tr>';
       $tasks = DB::table('tasks')->get();
       foreach ($tasks as $task) {
         echo '<tr>';
         echo '<td>' . $task->id . '</td><td><a href="/tasks/' . $task->id . '">' . $task->name . '</a></td><td>' . $task->counter . '</td>';
         echo '</tr>';
       }
-    echo '</table>;  
+    echo '</table>';  
     }
 }
