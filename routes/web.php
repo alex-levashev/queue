8dc58c044bed997e38fe781cd728e228ed3de877
Alexey Levashev <alex.levashev@gmail.com>
2018-11-28 21:29:40 +0100

    And once more

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,6 @@ Route::get('/tasks/{id}', function ($id) {
   $count += intval($count);
   DB::table('tasks')
    ->where('id',$id)
-   ->update(['counter' => strval($count));
+   ->update(['counter' => strval($count)]);
   // echo $count;
 });
